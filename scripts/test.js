9304202e2f0dc29f3bce60fc20afff8e98828102
Wisp <52381993+Wisp42@users.noreply.github.com>
2020-06-24 07:51:22 +0300

    tests pedi

---
+++ scripts/test.js
@@ -0,0 +1,82 @@
+var pedi_plus_db = [
+	{name: "Фізіологічні стани новонароджених", links: ["../books/pediatry_book.pdf#page=23"],},
+	{name: "Техніка проведення гірчичного обгортання", links: ["../pedi_pages/p65.html"], },
+	{name: "Анатомо-фізіологічні особливості новонародженого", links: ["../books/pediatry_book.pdf#page=21"],},
+	{name: "Уведення газовідвідної трубки", links: ["../pedi_pages/p47.html"], },
+	{name: "Характеристика та ознаки недоношеного новонародженого", links: ["../books/pediatry_book.pdf#page=31"],},
+	{name: "Проведення антропометрії", links: ["../pedi_pages/p16.html", "../pedi_pages/p17.html", "../pedi_pages/p18.html"], },
+	{name: "Функціональні особливості органів і систем недоношеного новонародженого. Вигодовування та зігрівання", links: ["../books/pediatry_book.pdf#page=32"],},
+	{name: "Визначення добової та разової кількості їжі дітям першого року життя", links: ["https://studfile.net/preview/5751522/page:8/"], },
+	{name: "Анатомо-фізіологічні особливості нервової системи. Нервово-психічний розвиток дитини", links: ["../books/pediatry_book.pdf#page="],},
+	{name: "Проведення підрахунку частоти дихання та пульсу, визначення їх характеру", links: ["../pedi_pages/p13.html"], },
+	{name: "Фізичний розвиток дитини", links: ["../books/pediatry_book.pdf#page=71"],},
+	{name: "Проведення вимірювання артеріального тиску та його реєстрація", links: ["../pedi_pages/p74.html"], },
+	{name: "Характеристика грудного вигодовування. Переваги грудного вигодовування. Види й терміни введення підгодовування", links: ["../books/pediatry_book.pdf#page=143"],},
+	{name: "Призначення і розрахунок доз ергокальциферолу з метою профілактики та лікування рахіту", links: ["https://studopedia.su/13_53190_algoritm-vikonannya-praktichnoi-navichki.html"], },
+	{name: "Характеристика змішаного вигодовування. Протипоказання й утруднення при грудному вигодовуванні", links: ["../books/pediatry_book.pdf#page=153"],},
+	{name: "Одягання новонароджених", links: ["https://mamaluk.ua/yakspovitinovonarodzhenogovpelushkunaradistinasovist/"], },
+	{name: "Харчування дітей віком понад 1 рік. Контроль за харчуванням у дитячому дошкільному закладі", links: ["../books/pediatry_book.pdf#page=161"],},
+	{name: "Проведення ранкового туалету дитини грудного віку", links: ["../pedi_pages/p20.html"], },
+	{name: "Характеристика штучного вигодовування. Молочні суміші. Гіпогалактія та її профілактика", links: ["../books/pediatry_book.pdf#page=151"],},
+	{name: "Проведення туалету пупкової ранки", links: ["../pedi_pages/p5.html"], },
+	{name: "Анатомо-фізіологічні особливості шкіри. Захворювання шкіри в новонародженого", links: ["../books/pediatry_book.pdf#page=68"],},
+	{name: "Вимірювання температури тіла", links: ["../pedi_pages/p61.html"], },
+	{name: "Хвороби пупка. Лікування. Догляд", links: ["../books/pediatry_book.pdf#page=168"],},
+	{name: "Проведення контрольного зважування", links: ["../pedi_pages/p16.html"], },
+	{name: "Пологові травми. Внутрішньочерепна пологова травма. Клініка. Лікування. Догляд. Профілактика", links: ["../books/pediatry_book.pdf#page=195"],},
+	{name: "Закапування крапель в ніс, вуха, очі", links: ["../pedi_pages/p68.html"], },
+	{name: "Асфіксія новонароджених. Клінічна картина. Лікування. Догляд. Профілактика ", links: ["../books/pediatry_book.pdf#page=175"],},
+	{name: "Накладання зігрівального компресу на вухо, грудну клітку", links: ["../pedi_pages/p72.html"], },
+	{name: "Гіповітамінози. Рахіт. Етіологія. Клінічна картина", links: ["../books/pediatry_book.pdf#page=201"],},
+	{name: "Поставлення лікувальної та очисної клізм", links: ["../pedi_pages/p55.html"], },
+	{name: "Анатомо-фізіологічні особливості кісткової системи. Профілактика і лікування хворих на рахіт", links: ["../books/pediatry_book.pdf#page=70"],},
+	{name: "Проведення промивання шлунка", links: ["../pedi_pages/p53.html"], },
+	{name: "Гострі розлади травлення у дітей раннього віку. Клінічна картина. Лікування. Профілактика. Догляд", links: ["../books/pediatry_book.pdf#page=218"],},
+	{name: "Проведення догляду за кувезом", links: ["../pedi_pages/p10.html"], },
+	{name: "Гіпокальціємічний синдром. Клінічна картина. Лікування. Профілактика", links: ["https://studfile.net/preview/5586157/page:8/"],},
+	{name: "Проведення інгаляцій киснем та лікарськими препаратами", links: ["https://studopedia.ru/17_26883_I-provedennya-IngalyatsIyi-kisnem-dItyam-rIznimi-metodami.html"], },
+	{name: "Аномалії конституції. Ексудативно-катаральний діатез. Клінічна картина. Лікування. Профілактика. Догляд", links: ["../books/pediatry_book.pdf#page=214"],},
+	{name: "Застосування міхура з льодом і грілки", links: ["../pedi_pages/p48.html"], },
+	{name: "Анатомо-фізіологічні особливості ротової порожнини. Стоматит. Клінічна картина. Лікування. Догляд. Профілактика", links: ["../books/pediatry_book.pdf#page=251"],},
+	{name: "Проведення ШВЛ і закритого масажу серця", links: ["https://studfile.net/preview/1902559/page:118"], },
+	{name: "Ентеробіоз. Клінічна картина. Лікування. Профілактика", links: ["https://studfile.net/preview/5586157/page:11/"],},
+	{name: "Розведення антибіотиків і розрахунок дози", links: ["../pedi_pages/p63.html"], },
+	{name: "Анатомо-фізіологічні особливості органів дихання. Стенозувальний ларинготрахеобронхіт. Етіологія. Клінічна картина. Невідкладна медична допомога", links: ["../books/pediatry_book.pdf#page=263"],},
+	{name: "Проведення контрольного зважування", links: ["../pedi_pages/p29.html"], },
+	{name: "Фолікулярна ангіна. Етіологія. Клінічна картина. Лікування. Догляд. Профілактика", links: ["../books/pediatry_book.pdf#page=264"],},
+	{name: "Узяття зіскрібків на ентеробіоз", links: ["../pedi_pages/p60.html"], },
+	{name: "Анатомо-фізіологічні особливості легень. Гостра пневмонія у дітей раннього віку. Етіологія, клінічна картина, лікування вогнищевої пневмонії. Догляд", links: ["../books/pediatry_book.pdf#page=272"],},
+	{name: "Гострий бронхіт. Етіологія. Клінічна картина. Лікування. Профілактика", links: ["../books/pediatry_book.pdf#page=268"],},
+	{name: "Узяття калу для копрологічного та бактеріологічного досліджень, на яйця гельмінтів", links: ["../pedi_pages/p57.html"], },
+	{name: "Особливості перебігу пневмонії у новонароджених і недоношених новонароджених. Етіологія. Клінічна картина. Лікування", links: ["../books/pediatry_book.pdf#page=272"],},
+	{name: "Визначення добової та разової кількості їжі дітям першого року життя", links: ["https://studfile.net/preview/5751522/page:8/"], },
+	{name: "Бронхіальна астма. Етіологія. Клінічна картина. Лікування. Профілактика. Диспансерний нагляд", links: ["../books/pediatry_book.pdf#page=280"],},
+	{name: "Проведення підрахунку частоти дихання та пульсу, визначення їх характеру", links: ["../pedi_pages/p12.html"], },
+	{name: "Анатомо-фізіологічні особливості органів кровообігу", links: ["../books/pediatry_book.pdf#page=79"],},
+	{name: "Природжені вади серця, судин. Етіологія. Клінічна картина. Лікування. Профілактика. Догляд", links: ["../books/pediatry_book.pdf#page=285"],},
+	{name: "Проведення вимірювання артеріального тиску та його реєстрація", links: ["../pedi_pages/p74.html"], },
+	{name: "Пологові травми. Внутрішньочерепна пологова травма. Клініка. Лікування. Догляд. Профілактика", links: ["../books/pediatry_book.pdf#page=195"],},
+	{name: "Поставлення лікувальної та очисної клізм", links: ["../pedi_pages/p56.html"], },
+	]
+
+function show_content_test(arr) {
+	var container = document.getElementById('container');
+	function k_counter(){
+			if (k+1 < 10){
+				return '0' + (k+1);
+			}else{
+				return k + 1;
+			}
+		};
+	for(var k = 0; k < arr.length; k++){
+		for(var t = 0; t < arr[k].links.length; t++){
+			if(arr[k].not_writed){
+					var link = '<div class="theme_item"><a href="' + arr[k].links[t] + '" class="description not_writed"><div class="num">' + k_counter() + '</div>' + arr[k].name + '</a></div>';
+				}else{
+					var link = '<div class="theme_item"><a href="' + arr[k].links[t] + '" class="description"><div class="num">' + k_counter() + '</div>' + arr[k].name + '</a></div>';
+				}
+		}
+		container.innerHTML += link;
+	}
+	console.log("wosrg")
+}
